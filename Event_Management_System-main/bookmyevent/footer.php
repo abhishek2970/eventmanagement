e8cdbfc8089be8f5c9079fce6dbc9fd4195969a9
abhishek2970 <107938450+abhishek2970@users.noreply.github.com>
2022-11-13 19:07:34 +0530

    Update footer.php

--- Event_Management_System-main/bookmyevent/footer.php
+++ Event_Management_System-main/bookmyevent/footer.php
@@ -12,8 +12,8 @@
 <body>
 	 <footer id="footer">
     <p style="color: white; font-weight: bold;
-    font-size: 20px;" class="pull-right"><a href="#">Back to top</a></p>
-    <p style="color:blue; text-align:center;font-size:20px" >BookMyEvent- Created by Vishal,Vaibav,Tarun</p>
+    font-size: 20px;padding: 20px;" class="pull-right"><a href="#">Back to top</a></p>
+    <h2 style="color:blue; text-align:center;font-size:20px" >BookMyEvent- Created Abhishek Walia and Akshat Jain</h2>
 </footer>
 </body>
 </html>
